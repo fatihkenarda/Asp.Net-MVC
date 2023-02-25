d849f454668097799b45745851180bdf6aaa7c92
Fatih <fatihk.96brs@gmail.com>
2023-02-25 19:26:58 +0300

    İstatistik

---
+++ UrunStokTakip/UrunStokTakip/Controllers/IstatistikController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UrunStokTakip.Models;
+
+namespace UrunStokTakip.Controllers
+{
+    [Authorize(Roles = "A")]
+    public class IstatistikController : Controller
+    {
+        TakipSistemiEntities db = new TakipSistemiEntities();
+        // GET: Istatistik
+        public ActionResult Index()
+        {
+            var kategori = db.Kategori.Count();
+            ViewBag.kategori = kategori;
+
+            var urun = db.Urun.Count();
+            ViewBag.urun = urun;
+
+            var satis = db.Satis.Count();
+            ViewBag.satis = satis;
+
+            var sepet = db.Sepet.Count();
+            ViewBag.sepet = sepet;
+
+            var kullanici = db.Kullanici.Count();
+            ViewBag.kullanici = kullanici;
+
+            return View();
+        }
+    }
+}
---
+++ UrunStokTakip/UrunStokTakip/Controllers/TumSatislarController.cs
@@ -0,0 +1,21 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UrunStokTakip.Models;
+
+namespace UrunStokTakip.Controllers
+{
+    public class TumSatislarController : Controller
+    {
+        // GET: TumSatislar
+        TakipSistemiEntities db = new TakipSistemiEntities();
+        [Authorize(Roles ="A")]
+        public ActionResult Index(int sayfa=1)
+        {
+            return View(db.Satis.ToList().ToPagedList(sayfa,5));
+        }
+    }
+}
